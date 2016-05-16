bcc415630b5219b1f47298e44d4dd7689aa55c2c
Nick Fausti <ncfausti@gmail.com>
2016-05-16 17:18:48 -0400

    updated db info in settings for develop branch

--- ritms/settings/base.py
+++ ritms/settings/base.py
@@ -91,18 +91,18 @@ WSGI_APPLICATION = 'ritms.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'wagtail_database',
-            'USER': 'db_user',
-            'PASSWORD': '2B0rn0t2b!',
-            'HOST': 'my-db-instance.cn03qarihltn.us-west-2.rds.amazonaws.com',
-            'PORT': '3306',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+#    'default': {
+#            'ENGINE': 'django.db.backends.mysql',
+#            'NAME': 'wagtail_database',
+#            'USER': 'db_user',
+#            'PASSWORD': '2B0rn0t2b!',
+#            'HOST': 'my-db-instance.cn03qarihltn.us-west-2.rds.amazonaws.com',
+#            'PORT': '3306',
+#        }
 }
 
 
