f49ad17fb028529bcb7724ecec26acc02c765a76
Nick Fausti <ncfausti@gmail.com>
2016-05-16 17:25:32 -0400

    updated wsgi file to work with local python server instead of mod_wsgi via apache

--- ritms/wsgi.py
+++ ritms/wsgi.py
@@ -8,22 +8,21 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
 import sys
-import site
+#import site
 
-site.addsitedir('/home/ec2-user/.virtualenvs/ritms/lib/python2.7/site-packages')
+#site.addsitedir('/home/ec2-user/.virtualenvs/ritms/lib/python2.7/site-packages')
 
 #root_path = os.path.abspath(os.path.split(__file__)[0])
 #sys.path.insert(0, os.path.join(root_path, 'ritms'))
 #sys.path.insert(0, root_path)
-sys.path.insert(0,"../")
-sys.path.insert(0,"../../")
-sys.path.append("/var/www/ritms")
-sys.path.append("/var/www/ritms/ritms")
+#sys.path.insert(0,"../")
+#sys.path.insert(0,"../../")
+#sys.path.append("/var/www/ritms")
+#sys.path.append("/var/www/ritms/ritms")
 
-activate_env=os.path.expanduser('/home/ec2-user/.virtualenvs/ritms/bin/activate_this.py')
-execfile(activate_env, dict(__file__=activate_env))
+#activate_env=os.path.expanduser('/home/ec2-user/.virtualenvs/ritms/bin/activate_this.py')
+#execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 
