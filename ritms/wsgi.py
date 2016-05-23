2ebf2d729440d26905ade28414124c2172f1daa2
Nick Fausti <ncfausti@gmail.com>
2016-05-23 15:08:39 -0400

    deleted pyc and changed wsgi to use django built in server

--- ritms/wsgi.py
+++ ritms/wsgi.py
@@ -17,13 +17,13 @@ site.addsitedir('/home/ec2-user/.virtualenvs/ritms/lib/python2.7/site-packages')
 #root_path = os.path.abspath(os.path.split(__file__)[0])
 #sys.path.insert(0, os.path.join(root_path, 'ritms'))
 #sys.path.insert(0, root_path)
-sys.path.insert(0,"../")
-sys.path.insert(0,"../../")
-sys.path.append("/var/www/ritms")
-sys.path.append("/var/www/ritms/ritms")
+#sys.path.insert(0,"../")
+#sys.path.insert(0,"../../")
+#sys.path.append("/var/www/ritms")
+#sys.path.append("/var/www/ritms/ritms")
 
-activate_env=os.path.expanduser('/home/ec2-user/.virtualenvs/ritms/bin/activate_this.py')
-execfile(activate_env, dict(__file__=activate_env))
+#activate_env=os.path.expanduser('/home/ec2-user/.virtualenvs/ritms/bin/activate_this.py')
+#execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 
